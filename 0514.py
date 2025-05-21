3eca3df5ed6f42bc631d06060995c44f9f6e2a79
MaxxHuang0801 <112845129+MaxxHuang0801@users.noreply.github.com>
2025-05-21 14:45:04 -0700

    2025.0521.1

--- 0514.py
+++ 0514.py
@@ -1,15 +1,16 @@
 import math
 
-x = input("enter first number: ")
-y = input("enter second number: ")
+n1 = float(input("enter first number: ")))
+n2 = float(input("enter second number: ")))
 
-
+print("the square root of", n1, "is", math.sqrt(n1))
+print("the square root of", n2, "is", math.sqrt(n2))
 
 print("the larger number is: ")
 
-if float(x) > float(y):
-    print(x)
-elif float(x) < float(y):
-    print(y)
+if float(n1) > float(n2):
+    print(n1)
+elif float(n1) < float(n2):
+    print(n2)
 else:
     print("there are same")
