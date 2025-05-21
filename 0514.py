0e9c5e8722adac09ff7e858cfcf09f72e63cf4c7
MaxxHuang0801 <112845129+MaxxHuang0801@users.noreply.github.com>
2025-05-21 14:22:17 -0700

    float number 比大小

---
+++ 0514.py
@@ -0,0 +1,15 @@
+import math
+
+x = input("enter first number: ")
+y = input("enter second number: ")
+
+
+
+print("the larger number is: ")
+
+if float(x) > float(y):
+    print(x)
+elif float(x) < float(y):
+    print(y)
+else:
+    print("there are same")
