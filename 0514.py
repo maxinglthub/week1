9a3ac367f1a5b8b11081f08712333c864efbfbd4
MaxxHuang0801 <112845129+MaxxHuang0801@users.noreply.github.com>
2025-05-21 15:01:42 -0700

    猜數字

--- 0514.py
+++ 0514.py
@@ -1,16 +1,14 @@
-import math
+import random
+num = random.randint(1,10)
 
-n1 = float(input("enter first number: ")))
-n2 = float(input("enter second number: ")))
+print("I have a sercrt number between 1 and 10")
+guess = int(input("guess my number"))
 
-print("the square root of", n1, "is", math.sqrt(n1))
-print("the square root of", n2, "is", math.sqrt(n2))
+if guess == num:
+    print("got it")
+elif guess < num:
+        print("too small")
+elif guess > num:
+        print("too high")
 
-print("the larger number is: ")
-
-if float(n1) > float(n2):
-    print(n1)
-elif float(n1) < float(n2):
-    print(n2)
-else:
-    print("there are same")
+print ("the number is", num)
