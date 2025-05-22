08a73ce77220a17d18dadadb1a34a299c4de4a89
MaxxHuang0801 <112845129+MaxxHuang0801@users.noreply.github.com>
2025-05-21 17:04:21 -0700

    last ver

--- 0514.py
+++ 0514.py
@@ -1,14 +1,33 @@
-import random
-num = random.randint(1,10)
+price = 0
+zone = int(input("Enter your number of zones:"))
+age = int(input("Enter your age:"))
 
-print("I have a sercrt number between 1 and 10")
-guess = int(input("guess my number"))
+if zone == 1:
+    price = 2.75
+elif zone == 2:
+    price = 3.25
+elif zone == 3:
+    price = 4.5
+else:
+    print("Pls enter a number between 1 and 3")
 
-if guess == num:
-    print("got it")
-elif guess < num:
-        print("too small")
-elif guess > num:
-        print("too high")
 
-print ("the number is", num)
+if age <= 18 or age >= 65:
+    price = price * 0.8
+
+print("Your ticket price is:", price)
+
+money = float(input("Enter your money:"))
+
+if money == 2.75:
+    print("zone 1 with no discount")
+elif money == 3.25:
+    print("zone 2 with no discount")
+elif money == 4.5:
+    print("zone 3 with no discount")
+elif money == 2.2:
+    print("zone 1 with discount")
+elif money == 2.6:
+    print("zone 2 with discount")
+elif money == 3.6:
+    print("zone 3 with discount")
